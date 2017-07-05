571cb29de995eee5a2cd40878693cdee2997cf39
Swarupa Vengurlekar <swarupa@creativecapsule.com>
2017-07-05 11:39:32 +0530

    dev changes

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         //Test11
+        // development changes
         return true
     }
 
